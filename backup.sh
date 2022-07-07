96ae8f4e303d0d8874f67cd044bae7015345a6d7
Michael Jordan <mjthecoder53161@gmail.com>
2022-07-07 11:50:27 +0900

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+DB_NAME="users"
+
+date=$(date '+%Y-%d-%m')
+
+# Create an array of tables
+TABLES=("customers" "employee" "offices" "offices" "orderdetails"
+"orders" "payments" "productlines" "products")
+
+
+backup() {
+    # This function loop through the TABLES and arrays 
+    # and create backup for each table.
+    for index in ${!TABLES[@]}; 
+        do
+            echo "Creating backup for $TABLES[$index]"
+            mysqldump -h localhost -u root -p'coolhand' ${DB_NAME} ${TABLES[$index]} > ${TABLES[$index]}_${date}.sql
+        done
+}
+
+restore() {
+    # Restores backup tables to the specified DB
+    DB_NAME="beta"
+    for index in ${!TABLES[@]}; 
+        do
+            echo "Restoring backup for $TABLES[$index]"
+            mysql -h localhost -u root -p'coolhand' ${DB_NAME} < ${TABLES[$index]}_2022-07-07.sql
+        done
+}
+
+backup
